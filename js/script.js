b8bbc10bb0b553efef11044392bd9444639a58a9
ZACKSHANE <zackyalhafizh1903@gmail.com>
2025-05-19 16:22:13 +0700

    update baru

--- js/script.js
+++ js/script.js
@@ -1,13 +1,41 @@
+// toggle class active untuk hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
 //ketika hamburger menu di klik//
 document.querySelector('#hamburger-menu').onclick =() => {
     navbarNav.classList.toggle('active');
 };
 
-//klik diluar sidebar untuk menghilangkan nav//
-const hamburger = document.querySelector('#hamburger-menu');
+// shoppingcart
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick =() =>{
+    shoppingCart.classList.toggle('active');
+    e.preventDefault();
+}
+
+
+//klik diluar sidebar untuk menghilangkan nav// 
+const hamburgerMenu = document.querySelector('#hamburger-menu');
+const searchButton = document.querySelector('#search-button'); 
 document.addEventListener('click',function(e){
-    if (!hamburger.contains(e.target)&& !navbarNav.contains(e.target)){
+    if (!hamburgerMenu.contains(e.target)&& !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active');
     }
-})
+    if (!searchButton.contains(e.target)&& !searchForm.contains(e.target)){
+        searchForm.classList.remove('active');
+    }
+});
+
+
+
+
+// toggle class active unutk search-form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document .querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+
+};
+
